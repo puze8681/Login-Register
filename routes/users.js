45fa37f2180d95312fd15d131cb1bf54ba7b5ce2
puze8681 <rsef0601@naver.com>
2017-01-30 16:47:47 +0900

    added success

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   Users.find(),function(err, users){
-    if(err) return res.status(500).send("DB err");
+    if(err) return res.status(500).send("DB err").json({success: "500"});
     if(users) return res. status(200).json({success: "200", user_id: users.user_id, user_password: users.user_password, user_name: users.user_name, user_age: users.user_age,token: users.token});
-    else return res.status(404).send("user not found");
+    else return res.status(404).send("user not found").json({success: "404"});
   }
 })
 
@@ -14,9 +14,9 @@ router.get('/', function(req, res, next) {
   var token = req.body.token;
 
   Users.remove({token: token}, function(err, users){
-    if(err) return res.status(500).send("DB err");
+    if(err) return res.status(500).send("DB err").json({success: "500"});
     if(users)  return res.status(200).json({success: "200", user_name: users.user_name, token: users.token});
-    else return res.status(404).send("token not found");
+    else return res.status(404).send("token not found").json(success: "404");
   });
 })
 
